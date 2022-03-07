2ee557ddabcc92012e9c9c5b60159452f346b8a6
Nandhini <nandhini.sridhar02@gmail.com>
2022-03-06 18:00:02 -0800

    adding new cc course and input for units

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict"
+//alert("Hello World");
+
+function addRow(){
+    console.log("entered add row function");
+    //let courses_cc = (document.getElementsByClassName("cc_left"))
+
+    //new form and new select
+    let new_form = document.createElement("form");
+    //new_form.setAttribute("name", "newForm");
+    let new_select = document.createElement("select");
+
+    //options for select
+    let new_option = document.createElement("option");
+    new_option.setAttribute("value", "default")
+    new_option.innerHTML = "Select A Class";
+    new_select.appendChild(new_option);
+
+    for(let i = 1; i < 5; i++){
+        let option_entry = document.createElement("option");
+        option_entry.innerHTML = "Class " + i;
+        new_select.appendChild(option_entry);
+    }
+
+    //add selections to form
+    new_form.appendChild(new_select);
+    document.getElementById("new_forms").appendChild(new_form); //add forms to left side
+
+    let new_input = document.createElement("input");
+    new_input.setAttribute("class", "units");
+    new_input.setAttribute("type", "text");
+    document.getElementById("new_units").appendChild(new_input);
+}
