9b7dab8d48b765ec8b9921dfad7793bf943c5f55
Nandhini <nandhini.sridhar02@gmail.com>
2022-03-06 18:18:17 -0800

    js for equivalent classes on right side and units home page

--- index.js
+++ index.js
@@ -30,4 +30,14 @@ function addRow(){
     new_input.setAttribute("class", "units");
     new_input.setAttribute("type", "text");
     document.getElementById("new_units").appendChild(new_input);
+
+    //adding equivalent classes to right side (4 year school)
+    let new_equiv = document.createElement("p");
+    new_equiv.innerHTML = "New Class";
+    document.getElementById("uni_courses").appendChild(new_equiv);
+
+    let new_units = document.createElement("p")
+    new_units.innerHTML = Math.ceil(Math.random() * 5);
+    document.getElementById("uni_units").appendChild(new_units);
+
 }
