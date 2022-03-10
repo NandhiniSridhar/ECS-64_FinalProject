766f45162690ca3928ecc2efb4dd8b851e8e6e86
Nandhini <nandhini.sridhar02@gmail.com>
2022-03-10 11:55:50 -0800

    working on chatbot styling

--- chat.js
+++ chat.js
@@ -2,6 +2,8 @@
 //alert("hello World");
 
 function revealText(){
+    hi = document.createElement("p");
+    hi.innerHTML = "Hello";
     chatbot = document.getElementById("reveal");
     //chatbot.setAttribute("visibility", "visible");
     //chatbot.style.visibility = "visible";
